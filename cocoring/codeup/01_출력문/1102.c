8b46ed00384ad44a46f4ee0b73f3bd18394980b4
cocoring <corock.dev@gmail.com>
2020-09-30 16:44:06 +0900

    Hello, World!

---
+++ cocoring/codeup/01_출력문/1102.c
@@ -0,0 +1,31 @@
+/**
+ * [1102](https://codeup.kr/problem.php?id=1102)
+ * Hello, World! (줄 바꿈 버전)
+ * 
+ * 1001번 문제를 다음과 같이 두 줄로 출력하세요.
+ * Hello,
+ * World!
+ * \n은 줄을 바꾸는 제어 문자이다. 이를 활용해보자.
+ * 
+ * 입력
+ * 없음
+ * 
+ * 출력
+ * Hello,
+ * World!
+ * 를 출력하시오
+ * 
+ * 입력 예시
+ * 
+ * 
+ * 출력 예시
+ * Hello,
+ * World!
+ */
+#include <stdio.h>
+
+int main()
+{
+    // escape sequence
+    return 0;
+}
